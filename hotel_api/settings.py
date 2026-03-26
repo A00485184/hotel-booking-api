118cccd3fe715aa343e5cbcc6abfcba1e8e8844e
md chistia chowdhury <chistia@gmail.com>
2026-03-25 22:45:37 -0300

    Fix: Explicitly disable DRF auth redirects

--- hotel_api/settings.py
+++ hotel_api/settings.py
@@ -113,6 +113,18 @@ USE_I18N = True
 
 USE_TZ = True
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': []
+}
+
+SWAGGER_SETTINGS = {
+    'USE_SESSION_AUTH': False,
+    'SECURITY_DEFINITIONS': None,
+}
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
