713fdb021e513934f7ec7bf35dc43cc35e12369c
md chistia chowdhury <chistia@gmail.com>
2026-03-25 21:09:20 -0300

    Feature: Create Reservation and Guest data models

--- reservations/models.py
+++ reservations/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+import string
+import random
 
-# Create your models here.
+def generate_confirmation_number():
+    length = 8
+    chars = string.ascii_uppercase + string.digits
+    return ''.join(random.choice(chars) for _ in range(length))
+
+class Reservation(models.Model):
+    confirmation_number = models.CharField(max_length=20, unique=True, default=generate_confirmation_number, primary_key=True)
+    hotel_name = models.CharField(max_length=255)
+    checkin = models.DateField()
+    checkout = models.DateField()
+
+    def __str__(self):
+        return self.confirmation_number
+
+class Guest(models.Model):
+    reservation = models.ForeignKey(Reservation, related_name='guests_list', on_delete=models.CASCADE)
+    guest_name = models.CharField(max_length=255)
+    gender = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.guest_name
